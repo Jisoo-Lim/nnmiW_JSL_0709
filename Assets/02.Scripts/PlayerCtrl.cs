2ac7e960e7f63020600ac7a9ecece313e71969ea
Bora Youn <100pour100@naver.com>
2021-07-02 16:40:23 +0900

    대중 기본 로직 구성했으나 실행이 안됨

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Transform tr;
+    private Animator animator;
+    private new Rigidbody rigidbody;
+
+
+    public float moveSpeed = 10.0f;
+    public float turnSpeed = 80.0f;
+    public float JumpPower = 5.0f;
+
+    // 델리게이트 선언
+    public delegate void PlayerDieHandler();
+    // 이벤트 선언
+    public static event PlayerDieHandler OnPlayerDie;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+
+        Debug.DrawRay(tr.position, -tr.up *1.0f, Color.green);
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = (Vector3.forward*v)+(Vector3.right*h);
+
+        tr.Translate(moveDir.normalized*moveSpeed*Time.deltaTime);
+
+        Animation(h,v);
+    }
+
+    void Animation(float h, float v)
+    {
+        if(Input.GetAxis("Horizontal")>=0.1f||Input.GetAxis("Horizontal")<=-0.1f||Input.GetAxis("Vertical")>=0.1f||Input.GetAxis("Vertical")<=-0.1f)
+        {
+            animator.SetBool("IsWalk", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalk", false);
+        }
+        // if(v>= 0.1f)
+        // {
+        //     // 전진 애니메이션
+        // }
+        // else if (v<=-0.1f)
+        // {
+        //     // 후진 애니메이션
+        // }
+        // else if (h>=0.1f)
+        // {   
+        //     // 오른쪽 이동 애니메이션
+        // }
+        // else if(h<=-0.1f)
+        // {
+        //     // 왼쪽 이동 애니메이션
+        // }
+        // else
+        // {
+        //     // Idle
+        // }
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        // // 충돌한 Collider가 키보드이면 인벤토리에 저장
+        // if(coll.CompareTag("KEYBOARD"))
+        // {
+        // }
+    }
+
+    void PlayerDie()
+    {
+        // 플레이어 사망 이벤트 호출(발생)
+        OnPlayerDie();
+    }
+}
--- Assets/02.Scripts/PublicCtrl.cs
+++ Assets/02.Scripts/PublicCtrl.cs
@@ -1,18 +1,165 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PublicCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 대중의 상태 정보
+    public enum State
     {
-        
+        IDLE,
+        WALK,
+        LOOK,
+        PASS
     }
 
-    // Update is called once per frame
+    // 대중의 현재 상태 
+    public State state  = State.IDLE;
+    // WALK 사정 거리
+    public float walkDist = 10.0f;
+    // LOOK 사정 거리
+    public float lookDist = 5.0f;
+    // 대중의 PASS 여부
+    public bool isPass = false;
+
+    private Transform publicTr;
+    private Transform playerTr;
+    private Transform doorTr;
+    private NavMeshAgent agent;
+    private Animator animator;
+    private RaycastHit hit;
+
+    private readonly int hashWalk  = Animator.StringToHash("IsWalk");
+    private readonly int hashLook   = Animator.StringToHash("IsLook");
+    private readonly int hashPass   = Animator.StringToHash("IsPass");
+
+    void OnEnable()
+    {
+        // 대중의 상태 체크
+        StartCoroutine(CheckPublicState());
+
+        // 상태에 따른 대중의 행동
+        StartCoroutine(PublicAction());
+    }  
+
+    void Awake()
+    {
+        publicTr = GetComponent<Transform>();
+        playerTr = GameObject.FindGameObjectWithTag("Player")?.GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        doorTr = GameObject.FindGameObjectWithTag("DOOR")?.GetComponent<Transform>();
+    }
+
+    void Start()
+    {
+        agent.updateRotation = false;
+    
+    }
+
     void Update()
     {
-        
+        Debug.Log($"대중 상태 : {state}");
+        if(agent.remainingDistance>=2.0f)
+        {
+            Vector3 direction = agent.desiredVelocity;
+
+            if(direction.sqrMagnitude >= 0.1f*0.1f)
+            {
+                Quaternion rot = Quaternion.LookRotation(direction);
+                publicTr.rotation = Quaternion.Slerp(publicTr.rotation, rot, Time.deltaTime*10.0f);
+            }
+        }
+    }
+
+    IEnumerator CheckPublicState()
+    {
+        while(!isPass)
+        {
+            yield return new WaitForSeconds(0.3f);
+
+            if(state == State.PASS) yield break;
+            
+            float distance = Vector3.Distance(playerTr.position, publicTr .position);
+
+            if(distance<=lookDist)
+            {
+                if(Physics.Raycast(playerTr.position, -playerTr.up, out hit, 1.0f, 1<<9))
+                {
+                    state = State.LOOK;
+                }
+            }
+            else if(distance<=walkDist)
+            {
+                state = State.WALK;
+            }
+            else
+            {
+                state = State.IDLE;
+            }
+        }
+    }
+
+    IEnumerator PublicAction()
+    {
+        while(!isPass)
+        {
+            switch(state)
+            {
+                case State.IDLE:
+                    agent.isStopped = true;
+                    animator.SetBool(hashWalk, false);
+                break;
+
+                case State.WALK:
+                    agent.SetDestination(doorTr.position);
+                    agent.isStopped = false;
+                    animator.SetBool(hashWalk, true);
+                    animator.SetBool(hashLook, false);
+                break;
+
+                case State.LOOK:
+                    publicTr.LookAt(playerTr.position);
+                    animator.SetBool(hashLook, true);
+                break;
+
+                case State.PASS:
+                    isPass = true;
+                    agent.isStopped = true;
+                    animator.SetTrigger(hashPass);
+                    GetComponent<CapsuleCollider>().enabled = false;
+                
+                yield return new WaitForSeconds(3.0f);
+
+                    GetComponent<CapsuleCollider>().enabled = true;
+                    this.gameObject.SetActive(false);
+                break;
+            }
+        yield return new WaitForSeconds(0.3f);
+
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.CompareTag("DOOR"))
+        {
+            state = State.PASS;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if(state == State.WALK)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, walkDist);
+        }
+        if(state == State.LOOK)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, lookDist);
+        }
     }
 }
